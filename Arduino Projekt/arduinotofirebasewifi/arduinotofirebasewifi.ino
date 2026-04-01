71a0d1c9f2d18ed9419a08c1649dcab08743a2d9
AntonN <anton.norling006@gmail.com>
2026-04-01 12:53:15 +0200

    arduino wifi connection
    arduinon kan ansluta till angivet wifi, eller internetdelning via telefon.

--- Arduino Projekt/arduinotofirebasewifi/arduinotofirebasewifi.ino
+++ Arduino Projekt/arduinotofirebasewifi/arduinotofirebasewifi.ino
@@ -1,10 +1,34 @@
 #include <WiFiS3.h>
+
+#define Wifi_SSID "Antons iPhone"
+#define Wifi_Password "12345678"
+
+#define server "bokningssystem-779e1-default-rtdb.europe-west1.firebasedatabase.app"
+
+WiFiClient client;
+
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin(9600);
+
+  Serial.println("Initializing!");
+
+  while (WiFi.begin(Wifi_SSID, Wifi_Password) != WL_CONNECTED) {
+    Serial.println(String("Connecting to wifi ") + Wifi_SSID);
+    delay(3000);
+  }
+
+  Serial.println(String("Connected to ") + Wifi_SSID);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+/*
+  if (client.connect(server, 443)) {
+
+    Serial.println("Connected to Firebase");
+  }
+  */
 
 }
