1eeb3bddcc5ec18adfe252f94d9e648996c27aec
AntonN <anton.norling006@gmail.com>
2026-04-06 10:27:37 +0200

    firebase firestore databas inegration
    arduino kan skicka samt hämta data från firebase firestore databasen

--- Arduino Projekt/arduinotofirebasewifi/arduinotofirebasewifi.ino
+++ Arduino Projekt/arduinotofirebasewifi/arduinotofirebasewifi.ino
@@ -1,51 +1,89 @@
 #include <WiFiS3.h>
-#include <Firebase.h>
+#include <HttpClient.h>
 
 #define WIfI_SSID "Antons iPhone"
 #define WIfI_PASSWORD "12345678"
+#include "projectsecrets.h"
 
-#define REFERENCE_URL "bokningssystem-779e1-default-rtdb.europe-west1.firebasedatabase.app"
-
-WiFiClient client;
-
-Firebase fb(REFERENCE_URL);
+WiFiSSLClient wificlient;
+//http://github.com/amcewen/HttpClient
+HttpClient client = HttpClient(wificlient, serverAddress, port);
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
 
+//Kopplar från föregående wifi och kopplar sedan till det nya. I detta fallet är det samma.
   WiFi.disconnect();
   Serial.println("Disconnecting from previous Wifi");
   delay(1000);
 
   Serial.println("Initializing!");
 
+//Medan arduino håller på att ansluta till wifi, skriv ut meddelanden i serial.
   while (WiFi.begin(WIfI_SSID, WIfI_PASSWORD) != WL_CONNECTED) {
     Serial.println(String("Connecting to wifi ") + WIfI_SSID);
     delay(3000);
   }
 
   Serial.println(String("Connected to ") + WIfI_SSID);
+
 }
 
-
-//https://github.com/Rupakpoddar/FirebaseArduino/blob/main/examples/Basic/Basic.ino
 void loop() {
   // put your main code here, to run repeatedly:
-   int responseCode;
+  //sendToFirestore();
 
-    // Example integer to send
-    int myValue = 3;  // replace this with your variable or sensor reading
+  readFromFirestore("test", "demo1"); //i firebase, (collection, document)
+  delay(10000); // skicka var 10 sek
 
-    // Push the integer to the "test" collection
-    responseCode = fb.pushInt("test", myValue);
+}
 
-    Serial.print("Pushed integer: ");
-    Serial.print(myValue);
-    Serial.print(" - Response Code: ");
-    Serial.println(responseCode);
+void sendToFirestore() {
+  String url = "/v1/projects/" + projectId + "/databases/(default)/documents/test/demo2?key=" + apiKey;
 
-    delay(2000); // Wait 2 seconds before sending the next value
+  String payload = R"(
+  {
+    "fields": {
+      "led": { "integerValue": "3" },
+      "date": { "stringValue": "arduino" }
+    }
+  }
+  )";
 
+  client.beginRequest();
+  //.post skapar nytt, .patch uppdaterar befintligt.
+  client.post(url);
+  client.sendHeader("Content-Type", "application/json");
+  client.sendHeader("Content-Length", payload.length());
+  client.beginBody();
+  client.print(payload);
+  client.endRequest();
+
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status: ");
+  Serial.println(statusCode);
+  Serial.println(response);
+}
+
+void readFromFirestore(String firebase_collection, String firebase_document) {
+  String url = "/v1/projects/" + projectId + "/databases/(default)/documents/" + firebase_collection + "/" + firebase_document + "?key=" + apiKey;
+
+//https://github.com/arduino-libraries/ArduinoHttpClient/blob/master/examples/SimpleGet/SimpleGet.ino
+  Serial.println("making GET request");
+  client.get(url);
+
+  // read the status code and body of the response
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  Serial.println("Wait five seconds");
+  delay(5000);
 }
